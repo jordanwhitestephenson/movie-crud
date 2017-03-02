27ff5a7690da6b29e9437d3490d66669a6646145
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-02 13:50:06 -0700

    4th Try

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes.js');
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 
 app.use(bodyParser.json());
